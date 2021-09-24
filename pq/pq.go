12cf59dae23967b1f15f36151e059f199ddc0e14
Miloš Hadžić <milos@rightfold.io>
2021-09-24 22:09:27 +0200

    Cleanup and some tests

--- pq/pq.go
+++ pq/pq.go
@@ -1,19 +1,43 @@
+/*
+
+A priority queue implementation to practice some Go. Mostly like the
+Skiena book implementation.
+
+Weird decision but I return -1, as a sort of "error" for pq.parent() but
+then real errors for Peek() etc. The current reasoning is that parent
+returns an int array index, for which -1 is obviously an invalid value.
+On the other hand Peek() returns any valid integer, so aside from
+min/max value, there's no integer value that could double as an
+error-like value.
+
+I could just use errors everywhere but I don't have a clear idea yet
+what the Go idioms are around errors and the -1 pattern is from Skiena's
+book.
+
+*/
 package pq
 
 const PQ_SIZE = 32
 
+// A min PriorityQueue/Heap.
 type PriorityQueue struct {
 	heap *[PQ_SIZE]int
 	n    int
 }
 
+// Init returns a pointer to a new PriorityQueue. It will be of capacity
+// PQ_SIZE. This is quite close to Skiena, but honestly I probably
+// should use slices and make the size either dynamic or capped at
+// initialization time.
 func Init() *PriorityQueue {
 	var heap [PQ_SIZE]int
 
 	return &PriorityQueue{&heap, 0}
 }
 
-// Inserts to the back of the queue and then bubbles up maintaining the invariant that parent value is always greater than that of the children.
+// Inserts to the back of the queue and then bubbles up maintaining the
+// invariant that parent value is always greater than that of the
+// children.
 func (q *PriorityQueue) Insert(x int) error {
 	if q.n >= PQ_SIZE {
 		return &QueueOverflowError{}
@@ -26,9 +50,61 @@ func (q *PriorityQueue) Insert(x int) error {
 	return nil
 }
 
+// Take returns the minimum element and removes it from the queue.
+func (q *PriorityQueue) Take() (int, error) {
+	if q.n == 0 {
+		return -1, &EmptyQueueError{}
+	}
+
+	val := q.heap[1]
+	q.heap[1] = q.heap[q.n]
+	q.n--
+	q.bubbleDown(1)
+
+	return val, nil
+}
+
+// Peek returns the min value without removing the element from the
+// queue.
 func (q *PriorityQueue) Peek() (int, error) {
 	if q.n == 0 {
 		return -1, &EmptyQueueError{}
+	}
+
+	return q.heap[1], nil
+}
+
+func (q *PriorityQueue) bubbleUp(p int) {
+	if parent(p) == -1 {
+		return
+	}
+
+	if q.heap[parent(p)] > q.heap[p] {
+		q.heap[parent(p)], q.heap[p] = q.heap[p], q.heap[parent(p)]
+
+		q.bubbleUp(parent(p))
+	}
+}
+
+func (q *PriorityQueue) bubbleDown(p int) (int, error) {
+	if q.n <= 0 {
+		return -1, &EmptyQueueError{}
+	}
+
+	childIndex := child(p)
+	minIndex := p
+
+	for i := 0; i <= 1; i++ {
+		if childIndex+i <= q.n {
+			if q.heap[minIndex] > q.heap[childIndex+i] {
+				minIndex = childIndex + i
+			}
+		}
+	}
+
+	if minIndex != p {
+		q.heap[minIndex], q.heap[p] = q.heap[p], q.heap[minIndex]
+		q.bubbleDown(minIndex)
 	}
 
 	return q.heap[1], nil
@@ -45,18 +121,6 @@ func parent(n int) int {
 
 func child(n int) int {
 	return n * 2
-}
-
-func (q *PriorityQueue) bubbleUp(p int) {
-	if parent(p) == -1 {
-		return
-	}
-
-	if q.heap[parent(p)] > q.heap[p] {
-		q.heap[parent(p)], q.heap[p] = q.heap[p], q.heap[parent(p)]
-
-		q.bubbleUp(parent(p))
-	}
 }
 
 type EmptyQueueError struct{}
--- pq/pq_test.go
+++ pq/pq_test.go
@@ -31,3 +31,43 @@ func TestPeek(t *testing.T) {
 		t.Fatalf("Expected Peek to return 1, got %d", val)
 	}
 }
+
+func TestTake(t *testing.T) {
+	pq := pq.Init()
+
+	_, err := pq.Take()
+
+	if err == nil {
+		t.Fatal("Expected Take to return an error when called on an empty Queue")
+	}
+
+	pq.Insert(5)
+
+	val, err := pq.Take()
+
+	if val != 5 {
+		t.Fatalf("Expected Take to return 5, got %d", val)
+	}
+
+	pq.Insert(1)
+	pq.Insert(3)
+
+	val, err = pq.Take()
+
+	if val != 1 {
+		t.Fatalf("Expected Take to return 1, got %d", val)
+	}
+
+	val, err = pq.Take()
+
+	if val != 3 {
+		t.Fatalf("Expected Take to return 3, got %d", val)
+	}
+
+	_, err = pq.Take()
+
+	if err == nil {
+		t.Fatal("Expected Take to return an error when called on an empty Queue")
+	}
+
+}
